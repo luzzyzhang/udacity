5f971ece7051b113201655c052928980f2601e9e
luzzy <luzzyzhang@gmail.com>
2016-07-20 22:30:08 +0800

    Add test() and hand_rank()

---
+++ cs212/poker.py
@@ -0,0 +1,30 @@
+# -*-coding: utf-8-*-
+
+
+def poker(hands):
+    """Return the best hand: poker([hand, ...]) => hand"""
+    return max(hands, key=hand_rank)
+
+
+def hand_rank(hand):
+    """Return a value indicating the ranking of hand."""
+    ranks = card_ranks(hand)
+    if straight(ranks) and flush(hand):
+        return 8
+    elif kind(4, ranks):
+        return 7
+    elif ...
+
+
+def test():
+    """Test cases for the functions in poker program"""
+    sf = "6C 7C 8C 9C TC".split()
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+    assert poker([sf, fk, fh]) == sf
+    assert poker([fk, fh]) == fk
+    assert poker([fh, fh]) == fh
+    assert poker([sf]) == sf
+    assert poker([sf] + 99*[fh]) == sf
+    return 'tests pass'
+print test()
