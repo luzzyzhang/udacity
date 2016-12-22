64993246f45850d59e7c89ded430dcf98493370d
luzzy <luzzyzhang@gmail.com>
2016-12-22 17:11:58 +0800

    Modify timedcall

--- design-computer-programs-cs212/lesson2/utils/timedcall.py
+++ design-computer-programs-cs212/lesson2/utils/timedcall.py
@@ -19,6 +19,24 @@ def timedcall(fn, *args):
     return t1-t0, result
 
 
+def average(numbers):
+    """Return the average (arithmetic mean) of a sequence of numbers."""
+    return sum(numbers) / float(len(numbers))
+
+
+def timedcalls(n, fn, *args):
+    """Call fn(*args) repeatedly: n times if n is an int, or up to
+    n seconds if n is a float; return the min, avg, and max time."""
+    if isinstance(n, int):
+        times = [timedcall(fn, *args)[0] for _ in range(n)]
+    else:
+        times = []
+        while sum(times) < n:
+            times.append(timedcall(fn, *args)[0])
+
+    return min(times), average(times), max(times)
+
+
 def time_cost(fn, *args, **kwargs):
     """Decerator method"""
     @wraps(fn)
