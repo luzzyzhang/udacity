3207573d7f1c863f17bdfaebb19cffbb3070f6ac
luzzy <luzzyzhang@gmail.com>
2017-01-11 18:13:01 +0800

    Lesson6

---
+++ design-computer-programs-cs212/lesson6/extend_prefix.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+# -----------------
+# User Instructions
+#
+# Write a function, extend_prefix, nested in find_words,
+# that checks to see if the prefix is in WORDS and
+# adds that to results if it is.
+#
+# If not, your function should check to see if the prefix
+# is in PREFIXES, and if it is should recursively add letters
+# until the prefix is no longer valid.
+
+
+import time
+
+
+def prefixes(word):
+    """A list of the initial sequences of a word,
+    not including the complete word."""
+    return [word[:i] for i in range(len(word))]
+
+
+def removed(letters, remove):
+    "Return a str of letters, but with each letter in remove removed once."
+    for L in remove:
+        letters = letters.replace(L, '', 1)
+    return letters
+
+
+def readwordlist(filename):
+    file = open(filename)
+    text = file.read().upper()
+    wordset = set(word for word in text.splitlines())
+    prefixset = set(p for word in wordset for p in prefixes(word))
+    return wordset, prefixset
+
+
+WORDS, PREFIXES = readwordlist('words4k.txt')
+
+
+# def find_words(letters):
+#     return extend_prefix('', letters, set())
+# 
+# 
+# def extend_prefix(pre, letters, results):
+#     if pre in WORDS:
+#         results.add(pre)
+#     if pre in PREFIXES:
+#         for L in letters:
+#             extend_prefix(pre+L, letters.replace(L, '', 1), results)
+#     return results
+
+
+def find_words(letters, pre='', results=None):
+    if results is None:
+        results = set()
+    if pre in WORDS:
+        results.add(pre)
+    if pre in PREFIXES:
+        for L in letters:
+            find_words(letters.replace(L, '', 1), pre+L, results)
+    return results
+
+
+def word_plays(hand, boad_letters):
+    """Find all word plays from hand that can be made to abut with a letter 
+    """
+    # Find prefix + L + suffix; L from brand_letters, rest from hand
+    results = set()
+    for pre in find_prefixes(hand, '', set()):
+        for L in board_letters:
+            add_suffixes(remove(hand, pre), pre+L, results)
+    return results
+
+
+def find_prefixes(hand, pre='', results=None):
+    """Find all prerixes (of words) that can be made from letters in hand.
+    """
+    if results is None:
+        results = set()
+    if pre in PREFIXES:
+        results.add(pre)
+        for L in hand:
+            find_prefixes(hand.replace(L, '', 1), pre+L, results)
+    return results
+
+
+def add_suffixes(hand, pre, results):
+    """Return the set of words that can be formed by extending pre with letters in hand.
+    """
+    # TODO
+
+
+def timedcall(fn, *args):
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, result
+
+
+hands = {  ## Regression test
+    'ABECEDR': set(['BE', 'CARE', 'BAR', 'BA', 'ACE', 'READ', 'CAR', 'DE', 'BED', 'BEE',
+         'ERE', 'BAD', 'ERA', 'REC', 'DEAR', 'CAB', 'DEB', 'DEE', 'RED', 'CAD',
+         'CEE', 'DAB', 'REE', 'RE', 'RACE', 'EAR', 'AB', 'AE', 'AD', 'ED', 'RAD',
+         'BEAR', 'AR', 'REB', 'ER', 'ARB', 'ARC', 'ARE', 'BRA']),
+    'AEINRST': set(['SIR', 'NAE', 'TIS', 'TIN', 'ANTSIER', 'TIE', 'SIN', 'TAR', 'TAS',
+         'RAN', 'SIT', 'SAE', 'RIN', 'TAE', 'RAT', 'RAS', 'TAN', 'RIA', 'RISE',
+         'ANESTRI', 'RATINES', 'NEAR', 'REI', 'NIT', 'NASTIER', 'SEAT', 'RATE',
+         'RETAINS', 'STAINER', 'TRAIN', 'STIR', 'EN', 'STAIR', 'ENS', 'RAIN', 'ET',
+         'STAIN', 'ES', 'ER', 'ANE', 'ANI', 'INS', 'ANT', 'SENT', 'TEA', 'ATE',
+         'RAISE', 'RES', 'RET', 'ETA', 'NET', 'ARTS', 'SET', 'SER', 'TEN', 'RE',
+         'NA', 'NE', 'SEA', 'SEN', 'EAST', 'SEI', 'SRI', 'RETSINA', 'EARN', 'SI',
+         'SAT', 'ITS', 'ERS', 'AIT', 'AIS', 'AIR', 'AIN', 'ERA', 'ERN', 'STEARIN',
+         'TEAR', 'RETINAS', 'TI', 'EAR', 'EAT', 'TA', 'AE', 'AI', 'IS', 'IT',
+         'REST', 'AN', 'AS', 'AR', 'AT', 'IN', 'IRE', 'ARS', 'ART', 'ARE']),
+    'DRAMITC': set(['DIM', 'AIT', 'MID', 'AIR', 'AIM', 'CAM', 'ACT', 'DIT', 'AID', 'MIR',
+         'TIC', 'AMI', 'RAD', 'TAR', 'DAM', 'RAM', 'TAD', 'RAT', 'RIM', 'TI',
+         'TAM', 'RID', 'CAD', 'RIA', 'AD', 'AI', 'AM', 'IT', 'AR', 'AT', 'ART',
+         'CAT', 'ID', 'MAR', 'MA', 'MAT', 'MI', 'CAR', 'MAC', 'ARC', 'MAD', 'TA',
+         'ARM']),
+    'ADEINRST': set(['SIR', 'NAE', 'TIS', 'TIN', 'ANTSIER', 'DEAR', 'TIE', 'SIN', 'RAD', 
+         'TAR', 'TAS', 'RAN', 'SIT', 'SAE', 'SAD', 'TAD', 'RE', 'RAT', 'RAS', 'RID',
+         'RIA', 'ENDS', 'RISE', 'IDEA', 'ANESTRI', 'IRE', 'RATINES', 'SEND',
+         'NEAR', 'REI', 'DETRAIN', 'DINE', 'ASIDE', 'SEAT', 'RATE', 'STAND',
+         'DEN', 'TRIED', 'RETAINS', 'RIDE', 'STAINER', 'TRAIN', 'STIR', 'EN',
+         'END', 'STAIR', 'ED', 'ENS', 'RAIN', 'ET', 'STAIN', 'ES', 'ER', 'AND',
+         'ANE', 'SAID', 'ANI', 'INS', 'ANT', 'IDEAS', 'NIT', 'TEA', 'ATE', 'RAISE',
+         'READ', 'RES', 'IDS', 'RET', 'ETA', 'INSTEAD', 'NET', 'RED', 'RIN',
+         'ARTS', 'SET', 'SER', 'TEN', 'TAE', 'NA', 'TED', 'NE', 'TRADE', 'SEA',
+         'AIT', 'SEN', 'EAST', 'SEI', 'RAISED', 'SENT', 'ADS', 'SRI', 'NASTIER',
+         'RETSINA', 'TAN', 'EARN', 'SI', 'SAT', 'ITS', 'DIN', 'ERS', 'DIE', 'DE',
+         'AIS', 'AIR', 'DATE', 'AIN', 'ERA', 'SIDE', 'DIT', 'AID', 'ERN',
+         'STEARIN', 'DIS', 'TEAR', 'RETINAS', 'TI', 'EAR', 'EAT', 'TA', 'AE',
+         'AD', 'AI', 'IS', 'IT', 'REST', 'AN', 'AS', 'AR', 'AT', 'IN', 'ID', 'ARS',
+         'ART', 'ANTIRED', 'ARE', 'TRAINED', 'RANDIEST', 'STRAINED', 'DETRAINS']),
+    'ETAOIN': set(['ATE', 'NAE', 'AIT', 'EON', 'TIN', 'OAT', 'TON', 'TIE', 'NET', 'TOE',
+         'ANT', 'TEN', 'TAE', 'TEA', 'AIN', 'NE', 'ONE', 'TO', 'TI', 'TAN',
+         'TAO', 'EAT', 'TA', 'EN', 'AE', 'ANE', 'AI', 'INTO', 'IT', 'AN', 'AT',
+         'IN', 'ET', 'ON', 'OE', 'NO', 'ANI', 'NOTE', 'ETA', 'ION', 'NA', 'NOT',
+         'NIT']),
+    'SHRDLU': set(['URD', 'SH', 'UH', 'US']),
+    'SHROUDT': set(['DO', 'SHORT', 'TOR', 'HO', 'DOR', 'DOS', 'SOUTH', 'HOURS', 'SOD',
+         'HOUR', 'SORT', 'ODS', 'ROD', 'OUD', 'HUT', 'TO', 'SOU', 'SOT', 'OUR',
+         'ROT', 'OHS', 'URD', 'HOD', 'SHOT', 'DUO', 'THUS', 'THO', 'UTS', 'HOT',
+         'TOD', 'DUST', 'DOT', 'OH', 'UT', 'ORT', 'OD', 'ORS', 'US', 'OR',
+         'SHOUT', 'SH', 'SO', 'UH', 'RHO', 'OUT', 'OS', 'UDO', 'RUT']),
+    'TOXENSI': set(['TO', 'STONE', 'ONES', 'SIT', 'SIX', 'EON', 'TIS', 'TIN', 'XI', 'TON',
+         'ONE', 'TIE', 'NET', 'NEXT', 'SIN', 'TOE', 'SOX', 'SET', 'TEN', 'NO',
+         'NE', 'SEX', 'ION', 'NOSE', 'TI', 'ONS', 'OSE', 'INTO', 'SEI', 'SOT',
+         'EN', 'NIT', 'NIX', 'IS', 'IT', 'ENS', 'EX', 'IN', 'ET', 'ES', 'ON',
+         'OES', 'OS', 'OE', 'INS', 'NOTE', 'EXIST', 'SI', 'XIS', 'SO', 'SON',
+         'OX', 'NOT', 'SEN', 'ITS', 'SENT', 'NOS'])}
+
+def test_words():
+    assert removed('LETTERS', 'L') == 'ETTERS'
+    assert removed('LETTERS', 'T') == 'LETERS'
+    assert removed('LETTERS', 'SET') == 'LTER'
+    assert removed('LETTERS', 'SETTER') == 'L'
+    t, results = timedcall(map, find_words, hands)
+    for ((hand, expected), got) in zip(hands.items(), results):
+        assert got == expected, "For %r: got %s, expected %s (diff %s)" % (
+            hand, got, expected, expected ^ got)
+    return t
+
+
+if __name__ == '__main__':
+    print test_words()
